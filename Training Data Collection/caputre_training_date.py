57ac73f2fc6ee1b8746e2dd5b177f5fb36533586
Frederik Riedel <frederik.riedel@frogg.io>
2018-02-05 17:01:48 +0100

    added inital version of the tracking script

---
+++ Training Data Collection/caputre_training_date.py
@@ -0,0 +1,20 @@
+from sneakysnek.recorder import Recorder
+from sneakysnek.mouse_event import MouseEvents
+import cv2
+import time
+
+camera = cv2.VideoCapture(0)
+
+def takePicture(clickEvent):
+	if clickEvent.event == MouseEvents.CLICK and clickEvent.direction == "DOWN":
+		print("clicked at: " + str(clickEvent.x) + ", " + str(clickEvent.y))
+		return_value, image = camera.read()
+		resized_image = cv2.resize(image, (500, 281)) 
+		cv2.imwrite('../data/'+str(time.time()) + 'opencv.png', resized_image)
+
+
+recorder = Recorder.record(takePicture)  # Replace print with any callback that accepts an 'event' arg
+# Some blocking code in your main thread...
+
+while True:
+	True
